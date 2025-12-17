259ede82f8db76654a71bc298bf31122854e40ab
akmake <92978132+akmake@users.noreply.github.com>
2025-12-17 17:37:47 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(
 );
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://passover1-1.onrender.com',
     credentials: true,
 }));
 
